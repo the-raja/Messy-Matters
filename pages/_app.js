2654858a1d9e364e2498b042f38355a514b8e3d3
the-raja <rrajakkumar01@gmail.com>
2025-12-13 13:47:42 +0530

    messy-matters

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }){
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Messy Matters</title>
+        <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <meta name="theme-color" content="#06b6d4" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
